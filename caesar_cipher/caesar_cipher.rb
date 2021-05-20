02e7282728fa958d2ce4d1d203651185e67be77b
James <jamesbasanmhs@gmail.com>
2021-05-20 12:13:45 -0700

    Refactor caesar cipher

---
+++ caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,23 @@
+class CaesarCipher
+    # Returns a string with each letter in plaintext replaced by a letter
+    # some fixed number of positions down the alphabet
+    def encrypt(plaintext, num_shift)
+        result = ""
+
+        # outputs an array containing characters converted to their ASCII value
+        plaintext.bytes.each do |ascii|
+            base = ascii <= 90 ? 65 : 97
+            # shift alphabet characters
+            if (ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)
+                # convert shifted ASCII value to its corresponding character
+                char = ((ascii - base + num_shift) % 26 + base).chr
+            # keep non-alphabet characters the same
+            else
+                # convert ASCII value to its corresponding character
+                char = ascii.chr
+            end
+            result << char
+        end
+        return result
+    end
+end
